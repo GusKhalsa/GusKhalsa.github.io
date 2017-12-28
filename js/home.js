130490fa7e72857559a5aae50ea7ff9a3872fe94
Gus Khalsa <guskhalsa@hotmail.com>
2017-12-28 01:30:11 +0000

    Made a few changes to the 'About Us' section, not final but temporary | Made nav transparent on load then have a background colour when scrolling

--- js/home.js
+++ js/home.js
@@ -12,20 +12,39 @@ app.controller('NavController', function($scope){
 app.controller('ProfileController', function($scope){
 	$scope.fullName = "Gursimran Khalsa";
 	$scope.shortName = "Gus";
+	$scope.profilepicture = "../img/profilepic1.JPG";
+	$scope.DOB = "07/08/1998";
+	$scope.email = "gk264@kent.ac.uk";
+	$scope.number = "07443036441";
 
 	$scope.education = [
-	{school: 'Northfleet Technology College', 
-	 subject: "12 GCSE's A*-C including Maths, English + 3 A Levels (Maths[B], Computing[B], BTEC Creative Media Production[Distinction*])",
+	{school: 'Northfleet Technology College',
+	 subject: "12 GCSE's A*-C including Maths and English",
 	 startDate: "Sep 2009",
+	 endDate: "June 2014"},
+	{school: 'Northfleet Technology College',
+	 subject: 'A Level Mathematics - B \n A Level Computing - B \n BTEC Level 3 Subsidairy Diploma Creative Media Production - Distinction*',
+	 startDate: "Sep 2014",
 	 endDate: "June 2016"},
 	{school: 'University of Kent', 
-	 subject: "BSc Computer Science",
+	 subject: "BSc Computer Science 1st Year",
 	 startDate: "Sep 2016", 
 	 endDate: "June 2020(Expected)"}
 	];
 
 	$scope.showModules = false;
-	$scope.modules = ['Intro to OOP', 'Further OOP', 'Foundations of Computing I', 'Databases and the Web'];
+	$scope.modules = ['Intro to Object Oriented Programming',
+					  'Human Computer Interaction',
+					  'Foundations of Computing I',
+					  'Computer Systems',
+					  'Further Object Oriented Programming',
+					  'Foundation of Computing II', 
+					  'People and Computing',
+					  'Databases and the Web',
+					  'Algorithms, Correctness and Efficiency', 
+					  'Web Development',
+					  'Theory of Computing',
+					  'Software Engineering'];
 
 	$scope.workExperience = [
 	{company: "Laptop World", 
@@ -44,7 +63,6 @@ app.controller('ProfileController', function($scope){
 	$scope.contacts = [
 		{icon: 'img/github.png', link: 'https://github.com/GusKhalsa'},
 		{icon: 'img/linkedin.png', link: 'LinkedIn'},
-		{icon: 'img/facebook.png', link: 'Facebook'},
-		{icon: 'img/email.png', link: 'Email'}
+		{icon: 'img/facebook.png', link: 'Facebook'}
 	];
 });
