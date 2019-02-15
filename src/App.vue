aae8a087fcbc44fc6783651fb9c1fab449ba225e
GusKhalsa <guskhalsa@hotmail.com>
2019-02-15 20:58:50 +0000

    Added nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div id="navigation">
+      <h1>Gus Khalsa</h1>
+      <nav class="dark-theme">
+        <ul>
+          <li><a href="#">HOME</a></li>
+          <li><a href="#">PROJECTS</a></li>
+          <li><a href="#">PORTFOLIO</a></li>
+          <li><img src="../src/assets/images/github-icon.png" alt="Github Icon" style="height: 40px;"></li>
+        </ul>
+      </nav>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -12,12 +22,34 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  /* border: 1px solid rgba(0, 0, 0, 0.3); */
+  font-family: 'Raleway', sans-serif;
+}
+
+#navigation {
+  background-color: black;
+}
+
+h1 {
+  float: left;
+}
+
+nav {
+  float: right;
+}
+
+ul {
+  list-style: none;
+}
+
+li {
+  display: inline;
+  padding: 1.5em;
+  font-size: 24px;
+}
+
+a {
+  text-decoration: none;
 }
 </style>
